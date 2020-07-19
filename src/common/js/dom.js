a78bb28bdb975593423a495b1071c6ca97ad9b62
lyx <651016060@qq.com>
2020-07-19 20:10:08 +0800

    char06 6 commit -- finish 7/7 music.vue

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -21,3 +21,35 @@ export function getData(el, name, val) {
         return el.getAttribute(name)
     }
 }
+
+// 自定义auto prefix 的方法
+const elementStyle = document.createElement('div').style
+// 定义一个自运行函数 拿到浏览器厂商名称
+const vendor = (() => {
+    const transformNames = {
+        webkit: 'webkitTransform',
+        Moz: 'MozTransform',
+        O: 'OTransform',
+        ms: 'msTransform',
+        standard: 'transform'
+    }
+    for (let key in transformNames) {
+        if (elementStyle[transformNames[key]] !== undefined) {
+            return key
+        }
+    }
+    return false
+})()
+
+export function prefixStyle(style) {
+    // 如果没有厂商浏览器错误
+    if (vendor === false) {
+        return false
+    }
+    // 如果厂商是这个标准模式,那么直接返回style
+    if (vendor === 'standard') {
+        return style
+    }
+    // 依据对应的厂商返回对应的拼接style
+    return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
