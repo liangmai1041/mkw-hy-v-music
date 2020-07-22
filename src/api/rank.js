3c59da6c47133533b0a05adf1961e79013edce73
lyx <651016060@qq.com>
2020-07-22 12:02:28 +0800

    char09 1 commit -- finish getTopList()&rank.vue (2/5)

---
+++ src/api/rank.js
@@ -0,0 +1,14 @@
+import { commonParams, options } from './config'
+import jsonp from 'common/js/jsonp'
+
+export function getTopList() {
+    const url = "https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg"
+
+    const data = Object.assign({}, commonParams, {
+        uin: 0,
+        needNewCode: 1,
+        platform: 'h5'
+    })
+
+    return jsonp(url, data, options)
+}
