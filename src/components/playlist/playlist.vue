09c9d5ada874e7dc9b4474752b8c4a7d34217619
lyx <651016060@qq.com>
2020-07-23 23:28:54 +0800

    char11 4 commit -- finish 2/6 add-song.vue (11/17)

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -30,7 +30,7 @@
                     </transition-group>
                 </scroll>
                 <div class="list-operate">
-                    <div class="add">
+                    <div class="add" @click="addSong">
                         <i class="icon-add"></i>
                         <span class="text">添加歌曲到队列</span>
                     </div>
@@ -40,6 +40,7 @@
                 </div>
             </div>
             <confirm ref="confirm" @confirm="confirmClear" text="是否清空播放列表" confirmBtnText="清空"></confirm>
+            <add-song ref="addSong"></add-song>
         </div>
     </transition>
 </template>
@@ -50,6 +51,7 @@ import {playMode} from 'common/js/config'
 import Scroll from 'base/scroll/scroll'
 import Confirm from 'base/confirm/confirm'
 import {playerMixin} from 'common/js/mixin'
+import AddSong from 'components/add-song/add-song'
 
 export default {
     mixins: [playerMixin],
@@ -109,6 +111,9 @@ export default {
             this.deleteSongList()
             this.hide()
         },
+        addSong() {
+            this.$refs.addSong.show()
+        },
         ...mapActions([
             'deleteSong',
             'deleteSongList'
@@ -122,7 +127,8 @@ export default {
     },
     components: {
         Scroll,
-        Confirm
+        Confirm,
+        AddSong
     }
 }
 </script>
